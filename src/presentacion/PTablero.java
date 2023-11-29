259f8f0673a77ab91652ce75955c306297d3d1fc
Juliana Cardenas <ljcardenass@udistrital.edu.co>
2023-11-29 03:59:54 -0500

    Ajuste clase PTablero

--- src/presentacion/PTablero.java
+++ src/presentacion/PTablero.java
@@ -1,45 +1,70 @@
 package presentacion;
-
+import logica.Othello;
 import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import logica.Othello;
-import logica.Ficha;
-
 public class PTablero extends JPanel implements MouseListener {
-	private Othello othello;
-	
-	public PTablero() {
+    private Othello othello;
+    private int[][] matrizJuego;
+
+    private int turno=2;
+    private FOthello fOthello;
+    public PTablero(Othello othello, FOthello fOthello) {
+        this.othello = othello;
+        this.fOthello = fOthello;
 		this.addMouseListener(this);
-		this.repaint();
-	}
 
-	public void paint(Graphics g) {
-	Graphics2D g2d = (Graphics2D)g;
-	g2d.setColor(new Color(13, 149, 70));
-	g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
+	  }
 	
-	g2d.setColor(Color.BLACK);
-	g2d.setStroke(new BasicStroke(3));
-	for(int i=this.getWidth()/8; i<this.getWidth(); i+=this.getWidth()/8) {
-		g2d.drawLine(i, 0, i, this.getHeight());
-	}
-	for(int i=this.getHeight()/8; i<this.getHeight(); i+=this.getHeight()/8) {
-		g2d.drawLine(0, i, this.getWidth(), i);
-	}
-}
+	
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setColor(new Color(250, 250, 250));
+        g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
+        g2d.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(1));
+        for(int i=this.getWidth()/8; i<this.getWidth(); i+=this.getWidth()/8) {
+            g2d.drawLine(i, 0, i, this.getHeight());
+        }
+        for(int i=this.getHeight()/8; i<this.getHeight(); i+=this.getHeight()/8) {
+            g2d.drawLine(0, i, this.getWidth(), i);
+        }
+
+        if (this.matrizJuego != null) {
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (this.matrizJuego[i][j] == 1) {
+                        Image img = new ImageIcon("img/fichaFNegra.png").getImage();
+                        g2d.drawImage(img, j * this.getWidth() / 8, i * this.getHeight() / 8, this.getWidth() / 8, this.getHeight() / 8, this);
+                    } else if (this.matrizJuego[i][j] == 2) {
+                        Image img = new ImageIcon("img/fichaFBlanca.png").getImage();
+                        g2d.drawImage(img, j * this.getWidth() / 8, i * this.getHeight() / 8, this.getWidth() / 8, this.getHeight() / 8, this);
+                    }
+                }
+            }
+        }
+    }
 
 
+	
+	
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
@@ -50,8 +75,38 @@ public class PTablero extends JPanel implements MouseListener {
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
+		int fila = e.getX();
+		int columna = e.getY();
+		int primeraCoordenada = fila / (this.getWidth() / 8);
+		int segundaCoordenada = columna / (this.getHeight() / 8);
+
+		if (getTurno()% 2 == 0) {
+			if (othello.movimientoCorrectoNegras(segundaCoordenada, primeraCoordenada)) {
+				
+				othello.ejecutarMovimientoNegras(segundaCoordenada, primeraCoordenada);
+				actualizarTablero();				
+				setTurno(getTurno() + 1);
+		        othello.actualizarMovimientosCorrectos(1, othello.posiblesNegras);
+		        othello.actualizarMovimientosCorrectos(2, othello.posiblesBlancos);
+		        fOthello.actualizarContador();
+
+				
+				}
+		} else {
+			if (othello.movimientoCorrectoBlancas(segundaCoordenada, primeraCoordenada)) {
+				
+				othello.ejecutarMovimientoBlancas(segundaCoordenada, primeraCoordenada);
+				actualizarTablero();
+				setTurno(getTurno() + 1);
+		        othello.actualizarMovimientosCorrectos(1, othello.posiblesNegras);
+		        othello.actualizarMovimientosCorrectos(2, othello.posiblesBlancos);
+		        fOthello.actualizarContador();
+			}
+			}
+
+		}
+
+	
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
@@ -71,5 +126,26 @@ public class PTablero extends JPanel implements MouseListener {
 		
 	}
 	
+		      
+	public void actualizarTablero() {
+	    this.matrizJuego = this.othello.getMatriz();
+	    this.repaint();
+	}
 
-}
+
+	public int getTurno() {
+		return turno;
+	}
+
+
+	public void setTurno(int turno) {
+		this.turno = turno;
+	}
+	
+	
+
+		
+		  }
+		  
+
+
