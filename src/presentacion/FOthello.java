bdaf380b1c59b7ae78bb698b286570da394463b9
Juliana Cardenas <ljcardenass@udistrital.edu.co>
2023-11-29 03:58:36 -0500

    completó clase FOthello

--- src/presentacion/FOthello.java
+++ src/presentacion/FOthello.java
@@ -1,42 +1,79 @@
 package presentacion;
-
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+
+import logica.Othello;
 
 public class FOthello extends JFrame {
-	private PTablero pTablero;
 	
-	public FOthello() {
-		this.setTitle("Othello POO 2023");
-		this.setSize(600,600);
+	
+	private PTablero pTablero;
+	private Othello othello;
+	private JLabel fichas;
+
+	
+	public static void main(String[] args) {
+		Othello othello = new Othello();
+        FOthello fOthello = new FOthello(othello);
+        fOthello.setVisible(true);
+        fOthello.pTablero.actualizarTablero();      
+    }
+	
+	
+	public FOthello(Othello othello) {
+		this.setTitle("Juego Othello :)");
+		this.setSize(800,800);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
+		this.fichas = new JLabel("                                    Fichas Blancas: "+ othello.contadorBlancas() + "        Fichas Negras : "+ othello.contadorNegras());
 		
-		JButton bIniciar = new JButton("Iniciar");
+		fichas.setFont(new Font("Arial", Font.PLAIN, 20));
+		
+		this.add(fichas, BorderLayout.SOUTH);
+		
+		JButton bIniciar = new JButton("Volver a iniciar");
+		bIniciar.setFont(new Font("Arial", Font.BOLD, 20));
+		bIniciar.setBackground(new Color(250, 200, 200));
+		
 		this.add(bIniciar, BorderLayout.NORTH);
+		
 		bIniciar.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
+		
+							
+		    @Override
+		 public void actionPerformed(ActionEvent e) {
+		   Othello othello2 = new Othello();
+		   FOthello fOthello = new FOthello(othello2);
+		   fOthello.setVisible(true);
+		   fOthello.pTablero.actualizarTablero();
+		   dispose();
+		    }
 		});
+
 		
-		this.pTablero = new PTablero();
-		this.add(this.pTablero, BorderLayout.CENTER);
-		
-	//	JOptionPane.showMessageDialog(this, "sfkjasfkjas");
+		this.othello = othello;
+        this.pTablero = new PTablero(this.othello, this);
+        this.pTablero.setVisible(true);
+        this.add(this.pTablero, BorderLayout.CENTER);	
+	}
+	
+	
+	public void actualizarContador() {
+	    
+		fichas.setText("                                    Fichas Blancas: "+ othello.contadorBlancas() + "        Fichas Negras : "+ othello.contadorNegras());
+
 	}
 
-	public static void main(String[] args) {
-		FOthello fOthello = new FOthello();
-		fOthello.setVisible(true);
-	}
+	
+
+	
+
 	
 }
